52cf6746f8932b9645b8a34fc021387996d1582c
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2025-06-03 18:22:22 +0530

    Add files via upload

---
+++ variable .py
@@ -0,0 +1,19 @@
+variable_name ='''Hello wo"rld'''
+print(variable_name)
+abc = "I am learning python"
+print(type(abc))
+print(type(variable_name))
+
+new_variable = str(2)
+print(new_variable)
+variable_name2= "hello city"
+print(variable_name2)
+
+print(variable_name,variable_name2)
+print( 'hello world'+ ' and ' + 'hello city' + "" + str (3))
+print(f"{variable_name} and {variable_name2} {new_variable}")
+
+empty_string = " "
+non_empty_string = " boooo"
+print(len(empty_string))
+print(len(non_empty_string))
