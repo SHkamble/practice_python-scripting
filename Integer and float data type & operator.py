05b6643a0c0ee3c1e620a31ffefa16721a524b00
sharda <sharada.kamble1999@gmail.com>
2025-06-03 18:16:36 +0530

    practice python

---
+++ Integer and float data type & operator.py
@@ -0,0 +1,28 @@
+#Operator
+first_num,second_num = 10, 3
+print(f'Sum:{first_num + second_num}')
+print(f'Substract:{first_num - second_num}')
+print(f'Multiply:{first_num * second_num}')
+print(f'Divide:{first_num/ second_num}')
+print(f'Reminder:{first_num % second_num}')
+print(f'Power:{first_num ** second_num}')
+print(f'Quotient:{first_num // second_num}')
+print(f'greater than:{first_num > second_num}')
+print(f'Less than:{first_num < second_num}')
+print(f'Equals to:{first_num == second_num}')
+print(f'Not equals to:{first_num != second_num}')
+
+first_num,second_num= 10,10.0
+print(f'Is  not identical:{first_num is not second_num}')
+print(f'IS not Identical:{first_num is not second_num}')
+print(f'Is identical:{first_num is second_num}')
+print(f'AND: {True and False}')
+print(f'or: {True or False}')
+print(f'AND: {not(True and False)}')
+
+
+
+print(type(10))
+print(type(10.0))
+
+
