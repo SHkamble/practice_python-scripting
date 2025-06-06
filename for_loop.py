24084110e97dcef32e5ea252c0591418d4a57289
Sharada Kamble <119296988+SHkamble@users.noreply.github.com>
2025-06-06 18:30:23 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,11 @@
+for variable in ["12",143,"sdetsdet"]:
+    print(variable)
+#
+# a = "shardul"
+# for item in range(10,20,2):
+#     print(a)
+#     print(item)
+
+a = "anshul"
+for char in a :
+    print(char)
